aexmod-tmp/platform_device_oneplus_enchilada

Add a KeyHandler for tri-state-key events
Change-Id: I22a7ee5b7eec5667b14f1a587a6d0138e77dc0f3
Signed-off-by: DennySPB <dennyspb@gmail.com>

KeyHandler/src/com/syberia/settings/device/KeyHandler.java
@@ -1,0 +1,73 @@
+/*
+ * Copyright (C) 2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.syberia.settings.device;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.os.Vibrator;
+import android.view.KeyEvent;
+
+import com.android.internal.os.DeviceKeyHandler;
+
+public class KeyHandler implements DeviceKeyHandler {
+    private static final String TAG = KeyHandler.class.getSimpleName();
+
+    // Slider key codes
+    private static final int MODE_NORMAL = 601;
+    private static final int MODE_VIBRATION = 602;
+    private static final int MODE_SILENCE = 603;
+
+    private final Context mContext;
+    private final AudioManager mAudioManager;
+    private final Vibrator mVibrator;
+
+    public KeyHandler(Context context) {
+        mContext = context;
+
+        mAudioManager = mContext.getSystemService(AudioManager.class);
+        mVibrator = mContext.getSystemService(Vibrator.class);
+    }
+
+    public KeyEvent handleKeyEvent(KeyEvent event) {
+        int scanCode = event.getScanCode();
+
+        switch (scanCode) {
+            case MODE_NORMAL:
+                mAudioManager.setRingerModeInternal(AudioManager.RINGER_MODE_NORMAL);
+                break;
+            case MODE_VIBRATION:
+                mAudioManager.setRingerModeInternal(AudioManager.RINGER_MODE_VIBRATE);
+                break;
+            case MODE_SILENCE:
+                mAudioManager.setRingerModeInternal(AudioManager.RINGER_MODE_SILENT);
+                break;
+            default:
+                return event;
+        }
+        doHapticFeedback();
+
+        return null;
+    }
+
+    private void doHapticFeedback() {
+        if (mVibrator == null || !mVibrator.hasVibrator()) {
+            return;
+        }
+
+        mVibrator.vibrate(50);
+    }
+}

AOSPA/android_hardware_oplus

KeyHandler/src/org/lineageos/settings/device/KeyHandler.java
@@ -1,0 +1,73 @@
+/*
+ * Copyright (C) 2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.device;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.os.Vibrator;
+import android.view.KeyEvent;
+
+import com.android.internal.os.DeviceKeyHandler;
+
+public class KeyHandler implements DeviceKeyHandler {
+    private static final String TAG = KeyHandler.class.getSimpleName();
+
+    // Slider key codes
+    private static final int MODE_NORMAL = 601;
+    private static final int MODE_VIBRATION = 602;
+    private static final int MODE_SILENCE = 603;
+
+    private final Context mContext;
+    private final AudioManager mAudioManager;
+    private final Vibrator mVibrator;
+
+    public KeyHandler(Context context) {
+        mContext = context;
+
+        mAudioManager = mContext.getSystemService(AudioManager.class);
+        mVibrator = mContext.getSystemService(Vibrator.class);
+    }
+
+    public KeyEvent handleKeyEvent(KeyEvent event) {
+        int scanCode = event.getScanCode();
+
+        switch (scanCode) {
+            case MODE_NORMAL:
+                mAudioManager.setRingerModeInternal(AudioManager.RINGER_MODE_NORMAL);
+                break;
+            case MODE_VIBRATION:
+                mAudioManager.setRingerModeInternal(AudioManager.RINGER_MODE_VIBRATE);
+                break;
+            case MODE_SILENCE:
+                mAudioManager.setRingerModeInternal(AudioManager.RINGER_MODE_SILENT);
+                break;
+            default:
+                return event;
+        }
+        doHapticFeedback();
+
+        return null;
+    }
+
+    private void doHapticFeedback() {
+        if (mVibrator == null || !mVibrator.hasVibrator()) {
+            return;
+        }
+
+        mVibrator.vibrate(50);
+    }
+}