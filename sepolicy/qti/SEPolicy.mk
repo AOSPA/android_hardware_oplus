37fd4e85ccbd73dacc2fcbbd497a6a12fee0447a
Vishalcj17 <vishalcj@aospa.co>
2023-01-16 16:42:57 +0900

    sepolicy: Define some rules as legacy.
    LAHAINA and above chipsets don't need
    definition of wakeup nodes as CLO does it in
    vendor sepolicy.
    
    Change-Id: I4f6d1b9825b6ab0b3a26763cb98f52875c035aa9

--- sepolicy/qti/SEPolicy.mk
+++ sepolicy/qti/SEPolicy.mk
@@ -6,3 +6,7 @@
 BOARD_VENDOR_SEPOLICY_DIRS += hardware/oplus/sepolicy/qti/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += hardware/oplus/sepolicy/qti/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += hardware/oplus/sepolicy/qti/public
+
+ifneq ($(call is-board-platform-in-list,$(5_4_FAMILY) $(5_10_FAMILY)),true)
+BOARD_VENDOR_SEPOLICY_DIRS += hardware/oplus/sepolicy/legacy/vendor
+endif
