a8669c3c7aba02a3c31ed2191199c17f41b4381c
Bruno Martins <bgcngm@gmail.com>
2023-10-26 11:32:37 +0100

    sepolicy: qti: Split sm8450 and sm8550 specific rules
    Follows the new logic of split sepolicy_vndr repos because there
    might be definitions set for a specific platform and not for the
    other(s).
    
    Change-Id: Icf37876a746907ecae07da73b4b11a0a7017cf4f

--- sepolicy/qti/SEPolicy.mk
+++ sepolicy/qti/SEPolicy.mk
@@ -6,8 +6,10 @@
 
 ifeq ($(call is-board-platform-in-list,$(4_14_FAMILY) $(4_19_FAMILY)),true)
     SEPOLICY_PLATFORM := legacy-um
-else
-    SEPOLICY_PLATFORM := um
+else ifeq ($(SEPOLICY_PATH), device/qcom/sepolicy_vndr/sm8450)
+    SEPOLICY_PLATFORM := sm8450
+else ifeq ($(SEPOLICY_PATH), device/qcom/sepolicy_vndr/sm8550)
+    SEPOLICY_PLATFORM := sm8550
 endif
 
 BOARD_VENDOR_SEPOLICY_DIRS += \
@@ -21,3 +23,14 @@ SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
     hardware/oplus/sepolicy/qti/public \
     hardware/oplus/sepolicy/qti/public/$(SEPOLICY_PLATFORM)
+
+ifneq ($(SEPOLICY_PLATFORM), legacy-um)
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    hardware/oplus/sepolicy/qti/vendor/common-um
+
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+    hardware/oplus/sepolicy/qti/private/common-um
+
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
+    hardware/oplus/sepolicy/qti/public/common-um
+endif
