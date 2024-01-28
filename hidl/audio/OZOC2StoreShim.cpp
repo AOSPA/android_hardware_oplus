be1aa95833d28acd5c686bb33ec3291d995b58ae
R0rt1z2 <me@r0rt1z2.com>
2024-01-28 14:07:51 +0100

    hidl: audio: Shim missing `C2PooledBlockPool` constructor
    * Commit 92573a2 [1] integrated support for AIDL-based BufferPool (C2PooledBlockPool)
      which required an extra parameter in the constructor to specify the version.
    
    * Since this change breaks compatibility with our old OZO blobs, provide a shim that calls
      the updated constructor with the default version parameter.
    
    * Force the shim to be only 32bit as our OZO blobs are strictly 32bit.
    
    - [1]: https://github.com/AOSPA/android_frameworks_av/commit/92573a2d1ada5ac2d374cc63799432f9ed97dfab
    
    Change-Id: I6d57dc55fce7b7b690ccef0ce6856eef5d74b98e

--- hidl/audio/OZOC2StoreShim.cpp
+++ hidl/audio/OZOC2StoreShim.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 Paranoid Android
+ * Copyright (C) 2024 Paranoid Android
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,6 +20,7 @@
 #include <C2AllocatorBlob.h>
 #include <C2AllocatorGralloc.h>
 #include <C2AllocatorIon.h>
+#include <C2BufferPriv.h>
 #include <C2PlatformStorePluginLoader.h>
 
 extern "C" c2_status_t
@@ -36,6 +37,19 @@ _ZN27C2PlatformStorePluginLoader15createAllocatorEjPNSt3__110shared_ptrI11C2Allo
       thisptr, allocatorId, allocator, [](C2BlockPool *ptr) { delete ptr; });
 }
 
+extern "C" void *
+_ZN17C2PooledBlockPoolC1ERKNSt3__110shared_ptrI11C2AllocatorEEyNS_13BufferPoolVerE(
+    void *thisptr, const std::shared_ptr<C2Allocator> &allocator,
+    const C2BasicGraphicBlockPool::local_id_t localId, C2PooledBlockPool::BufferPoolVer ver);
+
+extern "C" void *
+_ZN17C2PooledBlockPoolC1ERKNSt3__110shared_ptrI11C2AllocatorEEy(
+    void *thisptr, const std::shared_ptr<C2Allocator> &allocator,
+    const C2BasicGraphicBlockPool::local_id_t localId) {
+  return _ZN17C2PooledBlockPoolC1ERKNSt3__110shared_ptrI11C2AllocatorEEyNS_13BufferPoolVerE(
+      thisptr, allocator, localId, C2PooledBlockPool::VER_HIDL);
+}
+
 namespace android {
-    const char *MEDIA_MIMETYPE_AUDIO_OZOAUDIO = "audio/ozoaudio";
+const char *MEDIA_MIMETYPE_AUDIO_OZOAUDIO = "audio/ozoaudio";
 }
