8c471c50a1e74be09dd13268d6f392b30c370002
Mashopy <eliasgheeraert@gmail.com>
2024-07-09 23:25:07 +0200

    sepolicy: qti: Fixup 5.4 and newer families inclusion for SEPOLICY_PLATFORM logic
    Bengal, Lahaina and Holi platforms require a special fork of legacy-um folder as they already have
    some of legacy-um genfs_contexts rules defined on their platform folder at device/qcom/sepolicy_vndr repository
    
    Change-Id: I48cf9d3cb0dfbff9a38bc5234d3ead92ca74992d

--- sepolicy/qti/SEPolicy.mk
+++ sepolicy/qti/SEPolicy.mk
@@ -4,11 +4,17 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-ifeq ($(call is-board-platform-in-list,$(4_14_FAMILY) $(4_19_FAMILY)),true)
+ifeq ($(call is-board-platform-in-list,$(4_14_FAMILY) lito kona),true)
     SEPOLICY_PLATFORM := legacy-um
-else ifeq ($(SEPOLICY_PATH), device/qcom/sepolicy_vndr/sm8450)
+else ifeq ($(call is-board-platform,bengal),true)
+    SEPOLICY_PLATFORM := legacy-um-bengal
+else ifeq ($(call is-board-platform,holi),true)
+    SEPOLICY_PLATFORM := legacy-um-holi
+else ifeq ($(call is-board-platform,lahaina),true)
+    SEPOLICY_PLATFORM := legacy-um-lahaina
+else ifeq ($(call is-board-platform-in-list,$(5_10_FAMILY)),true)
     SEPOLICY_PLATFORM := sm8450
-else ifeq ($(SEPOLICY_PATH), device/qcom/sepolicy_vndr/sm8550)
+else ifeq ($(call is-board-platform-in-list,$(5_15_FAMILY)),true)
     SEPOLICY_PLATFORM := sm8550
 endif
 
@@ -24,7 +30,7 @@ SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
     hardware/oplus/sepolicy/qti/public \
     hardware/oplus/sepolicy/qti/public/$(SEPOLICY_PLATFORM)
 
-ifneq ($(SEPOLICY_PLATFORM), legacy-um)
+ifneq ($(SEPOLICY_PLATFORM), legacy-um legacy-um-bengal legacy-um-holi legacy-um-lahaina)
 BOARD_VENDOR_SEPOLICY_DIRS += \
     hardware/oplus/sepolicy/qti/vendor/common-um
 
@@ -33,4 +39,13 @@ SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
 
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
     hardware/oplus/sepolicy/qti/public/common-um
+else ifeq ($(SEPOLICY_PLATFORM), legacy-um legacy-um-bengal legacy-um-holi legacy-um-lahaina)
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    hardware/oplus/sepolicy/qti/vendor/common-legacy-um
+
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+    hardware/oplus/sepolicy/qti/private/common-legacy-um
+
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
+    hardware/oplus/sepolicy/qti/public/common-legacy-um
 endif
